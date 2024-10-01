9dadf970cb32dc81fb0226441bf4895de1dae884
Rayane Menkar <rayane.menkar@gmail.com>
2024-10-01 14:46:21 +0200

    ajout des dépendances maven et de la methode de connection

--- src/main/java/dataBase/dataBase.java
+++ src/main/java/dataBase/dataBase.java
@@ -1,13 +1,34 @@
 package dataBase;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class dataBase {
-    private String serveur = "localhost";
-    private String nomDeLaBase = "Todolist_JavaFx";
-    private String utilisateur;
-    private String motDePasse;
+        private String server = "localhost";
+        private String nomDeLaBase = "Todolist_JavaFx";
+        private String utilisateur = "root";
+        private String mdp = "";
+
+        public String getUrl() {
+            return "jdbc:mysql://" + server + "/" + nomDeLaBase + "?serverTimezone=UTC";
+        }
+
+        public Connection getConnection() {
+            try {
+                Connection cnx = DriverManager.getConnection(this.getUrl(), this.utilisateur, this.mdp);
+                System.out.print("État de la connexion :");
+                System.out.print(cnx.isClosed() ? "fermée" : "ouverte \r\n");
+                return cnx;
+            } catch (SQLException e) {
+                System.out.print("Erreur lors de la tentative de connexion à la base de données");
+                e.printStackTrace();
+                return null;
+            }
+        }
 
 
-    private String getUrl() {
-        return url;
     }
-}
+
+
+
